0f8f619ad4df197e63a24a27156637dedeb2b3f0
Nadia Perdiz <nadiaknperdiz@gmail.com>
2022-06-28 21:48:31 -0300

    Alterações para o heroku

--- projeto_integrador/settings.py
+++ projeto_integrador/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTHENTICATION_BACKENDS = (
         'django.contrib.auth.backends.ModelBackend',
     )
+
+django_heroku.settings(locals())
